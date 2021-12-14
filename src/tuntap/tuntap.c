ea23de85af7fc9fd782285aaf96ba69e67d6e5fd
孟洪平 <807753754@qq.com>
2021-12-14 18:22:16 +0800

    增加单元测试头文件 和增加添加网卡的命令

--- src/tuntap/tuntap.c
+++ src/tuntap/tuntap.c
@@ -6,12 +6,19 @@
 #include <fcntl.h>
 #include <memory.h>
 #include <stdio.h>
+#include <linux/if_tun.h>
+#include <sys/ioctl.h>
 #include "tuntap.h"
 #include "net/if.h"
+#include "utils.h"
 
-static int tun_fd;
+static int tun_fd=-1;
 static char* dev;
 
+static int set_route(char *dev,char *cidr)
+{
+	return run_cmd("ip route add dev %s %s", dev, cidr);
+}
 static int tun_alloc(char * dev,const char * tap_path)
 {
     struct ifreq ifr;
@@ -24,8 +31,40 @@ static int tun_alloc(char * dev,const char * tap_path)
         exit(1);
     }
     memset(&ifr,0,sizeof(struct ifreq));
+    ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
+    if(*dev)
+    {
+    	strncpy(ifr.ifr_name,dev,IFNAMSIZ);
+    }
+    //ioctl是设备驱动程序中对设备的I/O通道进行管理的函数
+    if((err =ioctl(fd,TUNSETIFF,(void *)&ifr)) <0)
+    {
+    	perror("ERR: Could not ioctl tun");
+	    close(fd);
+	    return err;
+    }
+
+	return fd;
 }
 
+int tun_read(char *buf,int len)
+{
+	if(tun_fd==-1)
+	{
+		perror("ERR: tun_fd not initialized");
+		return -1;
+	}
+	return read(tun_fd,buf,len);
+}
+int tun_write(char *buf,int len)
+{
+	if(tun_fd==-1)
+	{
+		perror("ERR: tun_fd not initialized");
+		return -1;
+	}
+	return write(tun_fd,buf,len);
+}
 void  tun_init(const char * tap_path)
 {
        dev = calloc(10,1);
---
+++ src/utils/utils.h
@@ -0,0 +1,11 @@
+//
+// Created by AT on 2021/12/14.
+//
+
+#ifndef TEST_UTILS_H
+#define TEST_UTILS_H
+
+#define CMDBUFLEN 100
+int run_cmd(char *cmd, ...);
+
+#endif //TEST_UTILS_H
